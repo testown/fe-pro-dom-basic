d1c78539389042aef03e801307ce9afdba1e32f0
yuriy <59261753+testown@users.noreply.github.com>
2022-07-13 17:41:38 +0300

    HW17 finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  let elem = document.body.querySelector('li');
+  let count = 1;
+  while (elem) {
+    if (count % 2) elem.style.backgroundColor = 'red';
+    elem = elem.nextElementSibling;
+    count++;
+  }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let elem = document.body.querySelector('li');
+  while (elem) {
+    if (elem.matches('.likedItem')) elem.style.backgroundColor = 'blue';
+    elem = elem.nextElementSibling;
+  }
+};
